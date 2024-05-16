1290675f8a5d9e9cce65d9875fb0a9837b8fd794
Samuel-vilela <146647141+Samuel-vilela@users.noreply.github.com>
2024-05-15 22:06:54 -0300

    Adicionar arquivos de projeto.

---
+++ atv.frank05/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace atv.frank05
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnChamar = new System.Windows.Forms.Button();
+            this.textBoxNome = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnChamar
+            // 
+            this.btnChamar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnChamar.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnChamar.Location = new System.Drawing.Point(275, 195);
+            this.btnChamar.Name = "btnChamar";
+            this.btnChamar.Size = new System.Drawing.Size(240, 41);
+            this.btnChamar.TabIndex = 0;
+            this.btnChamar.Text = "Mensagem";
+            this.btnChamar.UseVisualStyleBackColor = false;
+            this.btnChamar.Click += new System.EventHandler(this.btnChamar_Click);
+            // 
+            // textBoxNome
+            // 
+            this.textBoxNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxNome.Location = new System.Drawing.Point(275, 154);
+            this.textBoxNome.Name = "textBoxNome";
+            this.textBoxNome.Size = new System.Drawing.Size(240, 24);
+            this.textBoxNome.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(204, 154);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Nome:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxNome);
+            this.Controls.Add(this.btnChamar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnChamar;
+        private System.Windows.Forms.TextBox textBoxNome;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ atv.frank05/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace atv.frank05
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnChamar_Click(object sender, EventArgs e)
+        {
+            string nome = textBoxNome.Text;
+            string texto = ("Olá meu nome é:");
+            string mensagem = (texto + nome);
+            MessageBox.Show(mensagem); 
+        }
+    }
+}
